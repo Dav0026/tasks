94f03f3c3a14b6d6ea2d7083446ec35c2da9250d
David <david90178@gmail.com>
2022-06-30 16:21:00 +0400

    added function

---
+++ jsTasks.js
@@ -0,0 +1,29 @@
+function min(a,b){
+    if (a<b) return a;
+    else return b;
+}
+console.log(min(5,10));
+
+let minNum=function (c,d) {
+ if (c<d) return c;
+ else return d;
+}
+console.log(minNum(9,8))
+
+var maxNum=(a,b)=>{
+  if (a>b) return a;
+  return b;
+}
+console.log(maxNum(11,12))
+
+let a=1;
+let b=12;
+function calcFunc() {
+  return a + b
+}
+console.log(calcFunc())
+
+
+
+console.log(calcFunc());
+
